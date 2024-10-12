0449079376f91ebb37eea53009444d17ef701f8e
Ankit Sharma <ankit@nezubn.com>
2024-10-12 16:57:26 +0530

    operators liltorch

---
+++ liltorch/operators.py
@@ -0,0 +1,14 @@
+"""
+Collection of the core mathematical operators used throughout the code base.
+"""
+import math
+from typing import Callable, Iterable
+
+
+def mul(x: float, y: float) -> float:
+    return x * y
+    raise NotImplementedError('Not multiplying error')
+
+def id(x: float) -> float:
+    return x
+    raise NotImplementedError("ID not returned")
